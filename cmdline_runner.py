f0bad932556a8e581277ae7f05a3fbffb9ca0068
wiederin <wiederin@usc.edu>
2022-06-24 22:56:15 +0200

    init trainer

--- cmdline_runner.py
+++ cmdline_runner.py
@@ -4,11 +4,18 @@ import plotter as plt
 
 def main():
     df = dp.data_parser()
-    print("-------- Options --------")
-    print("0 plot")
-    selection = 0
-    if selection is 0:
-        plt.plotter(df)
+    while True:
+        print("---------- Options ----------")
+        print("0 plot")
+        print("1 train")
+        print("x exit")
+        selection = input()
+        if selection is '0':
+            plt.plotter(df)
+        elif selection is '1':
+            print("train")
+        elif selection is 'x':
+            break
 
 
 main()
